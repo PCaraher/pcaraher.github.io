9da8ec28464f05228aa5f44e7b0436cfc5875731
Unknown <caraherphilip@gmail.com>
2019-07-15 17:39:48 +0100

    Updated youtube embed to nocookie

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,6 +117,16 @@
 			}, 100);
 		});
 
+	// Cookie banner pop-up
+		$(document).ready(function(){   
+			setTimeout(function () {
+				$("#cookieConsent").fadeIn(200);
+		 	}, 2000);
+			$("#closeCookieConsent, .cookieConsentOK").click(function() {
+				$("#cookieConsent").fadeOut(200);
+			}); 
+		});
+
 	// Scrolly.
 		$('.scrolly').scrolly();
 
